594e4b482076156ce792fc1e8d7c9eb3e19f9708
Joedson Carvalho <carvalhojo881@gmail.com>
2026-04-25 19:56:18 -0300

    adicionando heranca

--- Projeto LookFor/src/entidade/Eletronico.java
+++ Projeto LookFor/src/entidade/Eletronico.java
@@ -1,15 +1,21 @@
 package entidade;
 
-public class Eletronico {
+public class Eletronico extends CadastrarItem{
 	
 		int garantia;
 		double modelo;
 		
-		public Eletronico(int garantia, double modelo) {
-			
+		public Eletronico(
+				int garantia, 
+				double modelo, 
+				String nome,
+				double preco,
+				String codItem,
+				String descItem) 
+		{
+			super(nome, preco, codItem, descItem);
 			this.garantia = garantia;
 			this.modelo = modelo;
-			
 		}
 
 }
