bad93d4fe434c99caeee873d0468787ebeb4e811
Pedro Luz <46757198+pedrohitalo@users.noreply.github.com>
2026-04-26 13:00:15 -0300

    Atualização Leitor txt

--- Projeto LookFor/src/entidade/CadastrarItem.java
+++ Projeto LookFor/src/entidade/CadastrarItem.java
@@ -16,7 +16,48 @@ public class CadastrarItem {
 		this.preco = preco;
 		this.codItem = codItem;
 		this.descricaoItem = descricaoItem; 
-		
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricaoItem() {
+		return descricaoItem;
+	}
+
+	public void setDescricaoItem(String descricaoItem) {
+		this.descricaoItem = descricaoItem;
+	}
+
+	@Override
+	public String toString() {
+		return "CadastrarItem [nome=" + nome + ", preco=" + preco + ", codItem=" + codItem + ", descricaoItem="
+				+ descricaoItem + "]";
+	}
+
+	
+	
+	
 
 }
---
+++ Projeto LookFor/src/tools/LeitorTxt.java
@@ -0,0 +1,47 @@
+package tools;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import entidade.CadastrarItem;
+
+public class LeitorTxt {
+	 public static void main(String[] args) {
+	 String path = "C:\\Users\\Admin\\Documents\\GitHub\\lookfor\\Projeto LookFor\\src\\data\\data.txt";
+	
+	 List<CadastrarItem> list = new ArrayList<CadastrarItem>();
+	 
+	 try (BufferedReader br = new BufferedReader (new FileReader(path))){
+
+		 String line = br.readLine();
+		 line = br.readLine();
+		 while (line != null) {
+			 String[] vect = line.split(",");
+			 String name = vect [0];
+			 Double price = Double.parseDouble(vect[1]);			 
+			 String qte = vect [2];
+			 String desc = vect[3];
+
+			 //String price = vect [1];
+			 //Integer qte = Integer.parseInt(vect[2]);
+			 
+			 CadastrarItem prod = new CadastrarItem(name, price, qte, desc);
+			 list.add(prod);
+			 
+			 line = br.readLine();
+		 }
+		 
+		 System.out.println("Produtos");
+		 for(CadastrarItem p : list) {
+			 System.out.println(p);
+			 
+		 }
+	 }
+	 catch (IOException e) {
+		 System.out.println("Error" + e.getMessage());
+	 }
+	 }
+}
